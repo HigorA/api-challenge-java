e79f8645db676484257954eaf72bf16899b7fff1
Higor <higor.a.santos12@hotmail.com>
2023-11-11 19:21:52 -0300

    Security Config update

--- src/main/java/br/com/fiap/challenge/ChallengeApplication.java
+++ src/main/java/br/com/fiap/challenge/ChallengeApplication.java
@@ -1,16 +1,9 @@
 package br.com.fiap.challenge;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @SecurityScheme(
